ab7908a64300be4db6a66d864ff7e716a6085b73
Bruno Benitez Gomez <2402137@utrivieramaya.edu.mx>
2026-07-25 09:35:06 -0500

    Ejercicios python

--- PracticaJS/Ejercicios2.js
+++ PracticaJS/Ejercicios2.js
@@ -105,3 +105,110 @@ const rawdividido1 = rawInput.slice(0, 4);
 const rawdividido2 = rawInput.slice(6, 11);
 const rawdividido3 = rawInput.slice(5, 6).toUpperCase();
 console.log(rawdividido1 + rawdividido3 +rawdividido2)
+
+//PROBLEMA 10
+const comment = "Este juego es malo, realmente malo.";
+// Tu tarea:
+// 1. Usa .replaceAll() para cambiar todas las ocurrencias de "malo" por "bueno".
+// 2. Imprime: "Comentario moderado: Este juego es bueno, realmente bueno."
+const comentariocensurado = comment.replaceAll("malo", "bueno");
+console.log(comentariocensurado);
+
+//PROBLEMA 11
+const symbol = "-";
+// Tu tarea:
+// 1. Crea una variable 'separator' usando .repeat() que cree una línea de 20 guiones.
+// 2. Crea un título: "--- BIENVENIDO ---"
+// 3. Imprime: 
+//    --------------------
+//    BIENVENIDO
+//    --------------------
+//    (Pista: Usa la variable 'separator' para las líneas superior e inferior).
+const separator = symbol.repeat(20);
+console.log(separator);
+console.log("BIENVENIDO");
+console.log(separator);
+
+//PROBLEMA 12
+const alertMessage = "Alerta: error en el sistema.";
+// Tu tarea:
+// 1. Reemplaza la palabra "error" por "fallo".
+// 2. Convierte toda la frase resultante a mayúsculas usando .toUpperCase().
+// 3. Imprime: "ALERTA: FALLO EN EL SISTEMA."
+const alertcambiado = alertMessage.replace("error", "fallo").toUpperCase();
+console.log(alertcambiado);
+
+//PROBLEMA 13
+const age = 17;
+const hasId = true;
+// Tu tarea:
+// Usa una estructura if/else con el operador lógico '&&' (AND).
+// Si tiene 18 años o más Y tiene identificación, imprime: "Puedes pasar."
+// Si no, imprime: "Acceso denegado."
+
+if(age >= 18 && hasId === true){
+    console.log("Puedes pasar.");
+} else {
+    console.log("Acceso denegado.");
+}
+
+//PROBLEMA 14
+const score = 85;
+//90 a 100: "A"
+//80 a 89: "B"
+//Menos de 80: "C"
+
+// Tu tarea:
+// Usa if, else if, y else para evaluar la variable 'score'.
+// Imprime la letra correspondiente según el rango.
+
+if (score >= 90 && score <= 100){
+    console.log("A")
+} else if(score >= 80 && score <= 89){
+    console.log("B")
+} else {
+    console.log("C")
+}
+
+//PROBLEMA 15
+const area = "Web"; // Puede ser "Web" o "Data"
+const experience = "Beginner"; // Puede ser "Beginner" o "Advanced"
+// Tu tarea:
+// Si el area es "Web" Y la experiencia es "Beginner", imprime: "Te recomiendo empezar con HTML/CSS."
+// Si el area es "Web" Y la experiencia es "Advanced", imprime: "Te recomiendo aprender React."
+// Si el area es "Data", imprime: "Te recomiendo aprender Python."
+// Si no es ninguna de las anteriores, imprime: "Area no reconocida."
+
+if(area === "Web" && experience === "Beginner"){
+    console.log("Te recomiendo empezar con HTML/CSS.")
+} else if(area === "Web" && experience === "Advanced"){
+    console.log("Te recomiendo aprender React.")
+} else if(area === "Data"){
+    console.log("Te recomiendo aprender Python.")
+} else {
+    console.log("Area no encontrada.")
+}
+
+//PROBLEMA 16
+// Tu tarea:
+// 1. Usa Math.random() para generar un número decimal.
+// 2. Transfórmalo a un número entero entre 1 y 6 usando Math.floor().
+// 3. Imprime: "Has sacado un: [resultado]"
+const dado = Math.floor(Math.random()*(6 - 1) - 1)
+console.log(`Has sacado un: ${dado}`);
+
+//PROBLEMA 17
+const price = 19.10;
+
+// Tu tarea:
+// 1. Usa Math.ceil() para redondear el precio hacia arriba.
+// 2. Imprime: "El precio final es: [resultado]"
+
+//PROBLEMA 18
+const userInput = 15; 
+// (Imagina que este valor cambia)
+
+// Tu tarea:
+// 1. Si userInput es mayor que 10, usa Math.min(userInput, 10) para forzar el máximo.
+// 2. Si userInput es menor que 1, usa Math.max(userInput, 1) para forzar el mínimo.
+// 3. Imprime el valor ajustado.
